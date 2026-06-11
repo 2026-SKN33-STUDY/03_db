c05d7190f80fca1939b6155ee224192592b57873
tangerine0101 <sejinjenny@gmail.com>
2026-06-11 17:29:41 +0900

    2026/06/11_sql, dql

--- 03_dql_select_2.sql
+++ 03_dql_select_2.sql
@@ -136,6 +136,96 @@ on
 order by
     a.emp_id asc;
 
-# cross join
+### menudb 계정
 
-# multiple join
+# cross join (카테시안곱, 곱집합)
+# 조인 되는 두 테이블의 모든 경우의 수를 처리한 것
+# 테스트를 위해 대량의 데이터가 필요할 때 / natural join 실패했을 때 cross join이 나타남 -> 실패 여부 확인
+select count(*) from tbl_menu;
+select count(*) from tbl_category;
+
+select
+    *
+from
+    tbl_menu a
+cross join
+    tbl_category;
+
+# self join
+# - 하나의 테이블에서
+# 한 행이 다른 행을 참조하는 관계가 있는 경우
+# 같은 테이블 끼리 조인하는 것
+# [tip] 똑같은 테이블이 2개 있다고 생각하면 쉬움
+select * from tbl_category; # 테이블 안에서 조인 확인
+
+select
+    child.category_code,
+    child.category_name,
+    parent.category_code as "상위 카테고리"
+from
+    tbl_category child
+join
+    tbl_category parent
+on
+    child.ref_category_code = parent.category_code
+where
+    parent.category_name = '식사';
+
+
+# multiple join (다중 조인)
+# - 3개 이상의 테이블을 조인하는 것
+# - join 순서가 매우 중요함!!!
+# 예시) a join b join c
+# -> 세 개가 한방에 조인되는 게 아니라
+# -> (a + b) join c
+# -> a + b + c
+# 속도가 느림
+
+select * from tbl_order;
+select * from tbl_order_menu;
+select * from tbl_menu;
+
+
+select
+     *
+from
+    tbl_order o
+join
+    tbl_order_menu om
+on
+    # o, om 합쳐진 relation 생성
+    o.order_code = om.order_code
+right join
+    tbl_menu m
+on
+    m.menu_code = om.menu_code;
+
+
+
+# employeedb 로 변경  schema 도 변경
+select * from employee;
+select * from department;
+select * from location;
+
+
+select
+    *
+from
+    employee e
+join
+    department d
+on
+    e.dept_code = d.dept_id    # location_id
+join
+    location l
+on
+    d.location_id = l.local_code
+
+
+
+
+
+
+
+
+
--- homework/02_select_homework_2.sql
+++ homework/02_select_homework_2.sql
@@ -14,4 +14,50 @@ WHERE
 
 ORDER BY
     HIRE_DATE DESC
-LIMIT 3
+LIMIT 3;
+
+
+
+
+
+# 2 join
+# 재직 중인 ‘대리’들의 직원명, 직급명, 급여, 사원번호, 이메일, 전화번호, 입사일을 출력하세요.
+# job_name = '대리'
+# 단, 급여를 기준으로 내림차순 출력하세요.
+select
+    e.EMP_NAME as 직원명,
+    j.JOB_NAME as 직급,
+    e.SALARY as 급여,
+    e.EMP_ID as 사원번호,
+    e.EMAIL,
+    e.PHONE as 전화번호,
+    e.HIRE_DATE as 입사일
+from
+    employee e
+join
+    job j
+on
+    e.job_code = j.job_code
+where
+    ENT_YN = 'N'
+and
+    JOB_NAME = '대리'
+order by salary desc;
+
+select
+    *
+from employee e
+
+
+
+
+
+
+
+
+
+
+
+
+
+
